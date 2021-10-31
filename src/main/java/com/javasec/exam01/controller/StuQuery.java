550311264b0a1f61a9406c3d355725e5a9c36b00
WinterBoot <WinterBoot@WinterBoot.dev>
2021-10-31 16:23:33 +0800

    更新数据库，请重新创建smartschool数据库，SQL文件在Resource文件夹中
    完成的功能：
    - Thymeleaf common公共页面的提取
    - BootStrap cover主题的整合
    - 网站整体框架的功能规划
    - 首页各功能的html模版和Controller跳转效果
    - 完善admin、main界面跳转
    - 学生查询：不安全的SQL注入
    - 教师查询：安全的查询操作
    - 留言板：不安全的XSS

---
+++ src/main/java/com/javasec/exam01/controller/StuQuery.java
@@ -0,0 +1,30 @@
+package com.javasec.exam01.controller;
+
+import com.javasec.exam01.entity.User;
+import com.javasec.exam01.service.StuQueryService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
+
+/**
+ * (Teacher)表控制层
+ *
+ * @author JavaExam
+ * @since 2021-10-31 06:57:09
+ */
+@Controller
+public class StuQuery {
+
+    @Resource
+    private StuQueryService stuQueryService;
+
+    @GetMapping("/stuQueryResult")
+    public String selectOne(String id, Model model) throws Exception{
+        User user = stuQueryService.selectUserByStatement(id);
+        model.addAttribute("user", user);
+        return "stuQueryResult";
+    }
+
+}
